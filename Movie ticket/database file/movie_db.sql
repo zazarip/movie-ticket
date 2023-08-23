3c5d84db9e3a9e56dba6dc9a39ab5042598f00fb
zazarip <106441151+zazarip@users.noreply.github.com>
2023-08-23 21:44:27 +0800

    Update movie_db.sql

--- Movie ticket/database file/movie_db.sql
+++ Movie ticket/database file/movie_db.sql
@@ -1,21 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2022 at 11:00 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.0.13
+-- Generation Time: Aug 23, 2023 at 03:42 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `movie_db`
@@ -62,8 +56,14 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`booking_id`, `booked_seats`, `user_id`, `movie_name`, `show_time`, `booking_time`, `is_cancel`) VALUES
-(8, 'a:2:{i:0;s:2:\"B2\";i:1;s:2:\"B4\";}', 2, 'PK Full Movie', '03:30 PM', '2022-06-26 08:10:29', 0),
-(13, 'a:1:{i:0;s:2:\"F2\";}', 2, 'Runway 34', '10:30 AM', '2022-07-15 07:31:31', 0);
+(8, 'a:2:{i:0;s:2:\"B2\";i:1;s:2:\"B4\";}', 2, 'PK Full Movie', '03:30 PM', '2023-01-25 16:52:41', 1),
+(13, 'a:1:{i:0;s:2:\"F2\";}', 2, 'Runway 34', '10:30 AM', '2023-01-25 16:52:46', 1),
+(14, 'a:2:{i:0;s:2:\"F9\";i:1;s:3:\"F10\";}', 2, 'Projek: High Council', '10:00 PM', '2023-01-25 16:52:20', 0),
+(15, 'a:1:{i:0;s:3:\"D10\";}', 6, 'Projek: High Council', '10:00 PM', '2023-01-26 06:02:26', 1),
+(16, 'a:2:{i:0;s:3:\"F11\";i:1;s:3:\"F12\";}', 6, 'Projek: High Council', '10:00 PM', '2023-01-26 06:06:29', 0),
+(17, 'N;', 2, 'Projek: High Council', '10:00 PM', '2023-01-26 06:07:57', 0),
+(18, 'a:1:{i:0;s:2:\"F6\";}', 6, 'Black Adam', '7:30 PM', '2023-01-26 17:39:37', 0),
+(19, 'a:1:{i:0;s:2:\"A4\";}', 7, 'Cruella', '10:30 AM', '2023-08-23 12:55:17', 1);
 
 -- --------------------------------------------------------
 
@@ -85,14 +85,15 @@ CREATE TABLE `movies` (
 --
 
 INSERT INTO `movies` (`id`, `name`, `description`, `link`, `logo`, `time`) VALUES
-(1, 'Sooryavanshi Movie', 'Sooryavanshi is a 2021 Indian Hindi-language action film written and directed by Rohit Shetty and produced by Reliance Entertainment, Rohit Shetty Picturez, Dharma Productions and Cape Of Good Films, based on a screenplay by Yunus Sajawal and an original story by Shetty. The fourth installment of Shetty\'s Cop Universe, it stars Akshay Kumar as the main protagonist, playing the role of ATS chief DCP Veer Sooryavanshi opposite Katrina Kaif and Jackie Shroff with supporting cast of Jaaved Jaaferi, Vivan Bhatena, Niharica Raizada, Gulshan Grover, Abhimanyu Singh, Sikandar Kher and Nikitin Dheer. Ajay Devgn and Ranveer Singh appear in extended cameos, reprising their roles of Singham and Simmba from the franchise\'s previous films, while Jackie Shroff portrays the main antagonist, playing the role of Omar Hafeez, a terrorist.', 'https://www.youtube.com/watch?v=u5r77-OQwa8', 'movie1.jpg', '7:30 PM'),
-(2, 'Runway 34', 'Runway 34 is a 2022 Indian Hindi-language thriller film directed by Ajay Devgn and produced by himself under his banner Ajay Devgn FFilms, the film stars Ajay Devgn, Amitabh Bachchan and Rakul Preet Singh with Boman Irani, Angira Dhar and Aakanksha Singh playing pivotal roles.[5]\r\n\r\nRunway 34 was released theatrically on 29 April 2022, coinciding with Eid.', 'https://www.youtube.com/watch?v=Lb8mQCpZHco', 'movie2.jpg', '10:30 AM'),
-(3, 'Scam 1992: The Harshad Mehta Story', 'Scam 1992: The Harshad Mehta Story is an Indian Hindi-language financial thriller streaming television series on SonyLIV directed by Hansal Mehta, with Jai Mehta serving as the co-director.[1] Based on 1992 Indian stock market scam committed by many stockbrokers including Harshad Mehta, the series is adapted from journalist Sucheta Dalal and Debashish Basu\'s 1992 book The Scam: Who Won, Who Lost, Who Got Away.[2] The screenplay and dialogues were written by Sumit Purohit, Saurabh Dey, Vaibhav Vishal and Karan Vyas.\r\n\r\nIt stars Pratik Gandhi as the protagonist Harshad Mehta, with Shreya Dhanwanthary appearing in a parallel lead role, as Sucheta Dalal. The series also features Satish Kaushik, Anant Mahadevan, Rajat Kapoor, K K Raina, Lalit Parimoo, Hemant Kher and Nikhil Dwivedi in pivotal roles. The series focuses on Harshad Mehta\'s life story, his meteoric rise and the subsequent downfall.', 'https://www.youtube.com/watch?v=BLeOcCeqsfI', 'movie3.jpg', '10:00 PM'),
-(4, 'Taare Zameen Par', 'Taare Zameen Par (transl. Stars on earth), also known as Like Stars on Earth in English, is a 2007 Indian Hindi-language drama film produced and directed by Aamir Khan. The film stars Khan himself, along with Darsheel Safary, Tanay Chheda, Sachet Engineer, Vipin Sharma and Tisca Chopra. The film explores the life and imagination of Ishaan (Safary), an 8-year-old dyslexic child. Although he excels in art, his poor academic performance leads his parents to send him to a boarding school. Ishaan\'s new art teacher Nikumbh suspects that he is dyslexic and helps him to overcome his reading disorder.\r\n\r\nCreative director and writer Amole Gupte initially developed the idea with his wife Deepa Bhatia, who served as the film\'s editor. Shankar–Ehsaan–Loy composed the film\'s score, and Prasoon Joshi wrote the lyrics for many of the songs. Principal photography took place in Mumbai and in Panchgani\'s New Era High School, and some of the school\'s students make appearances.', 'https://www.youtube.com/watch?v=F-PAI2HnQUo', 'movie4.jpg', '12:30 PM'),
-(5, 'Dilwale Dulhania Le Jayenge', 'Dilwale Dulhania Le Jayenge (transl. \"The Big-Hearted Will Take the Bride\"), also known by the initialism DDLJ, is a 1995 Indian Hindi-language musical romance film written and directed by Aditya Chopra in his directorial debut and produced by his father Yash Chopra. Released on 20 October 1995, the film stars Shah Rukh Khan and Kajol. The plot revolves around Raj and Simran, two young non-resident Indians, who fall in love during a vacation through Europe with their friends. Raj tries to win over Simran\'s family so the couple can marry, but Simran\'s father has long since promised her hand to his friend\'s son. The film was shot in India, London, and Switzerland, from September 1994 to August 1995.\r\n\r\nWith an estimated total gross of ₹2 billion[4][5] ($60 million) worldwide,[6] Dilwale Dulhania Le Jayenge was the highest-grossing Indian film of 1995 and one of the most successful Indian films in history. It won 10 Filmfare Awards—the most for a single film at that time—and the National Film Award for Best Popular Film Providing Wholesome Entertainment. ', 'https://www.youtube.com/watch?v=cmax1C1p660', 'movie5.jpg', '12:30 AM'),
-(6, 'PK Full Movie', 'PK (transl. Tipsy) is a 2014 Indian Hindi-language comedy-drama film[a] edited and directed by Rajkumar Hirani and written by Hirani and Abhijat Joshi, and jointly produced by Hirani and Vidhu Vinod Chopra under the banners Rajkumar Hirani Films and Vinod Chopra Films respectively. The story follows an alien who comes to Earth on a research mission, only for the remote control for his spaceship to be stolen and sold to a godman; he befriends a television journalist and in his quest to retrieve the remote, questions religious dogmas and superstitions. The film stars Aamir Khan in the titular role with Anushka Sharma, Sushant Singh Rajput, Sanjay Dutt, Boman Irani and Saurabh Shukla in pivotal roles.\r\n\r\nAfter the success of 3 Idiots (2009), Hirani and Joshi began scripting their next project; finding similarities with the plot of Inception (2010), they scrapped the film. It was later rewritten with a different angle and tone. During production, the film was initially titled Talli and later Ek Tha Talli before being changed to PK as the latter title was found to be too similar to', 'https://www.youtube.com/watch?v=SOXWc32k4zA', 'movie6.jpg', '3:30 PM'),
-(7, 'Dangal Full Movie', 'Dangal (transl. \"Wrestling Competition\") is a 2016 Indian Hindi-language biographical sports drama film directed by Nitesh Tiwari and produced by Aamir Khan and Kiran Rao under Aamir Khan Productions with Siddharth Roy Kapur under The Walt Disney Company India. The film stars Khan as Mahavir Singh Phogat, a pehlwani amateur wrestler who trains his daughters Geeta Phogat and Babita Kumari to become India\'s first world-class female wrestlers.[9] Fatima Sana Shaikh and Sanya Malhotra portray the adult versions of the two Phogat sisters, Zaira Wasim and Suhani Bhatnagar their younger versions, Sakshi Tanwar their mother, and Aparshakti Khurana their cousin, all of them except Tanwar in their film debuts.\r\n\r\nDevelopment on the film began in early 2013 when Tiwari began writing the script. Khan had interviewed the Phogat sisters in 2014 on his talk show Satyamev Jayate, before Tiwari approached him with the script months later, after which Khan became the lead actor and producer. Set primarily in the Indian state of Haryana,', 'https://www.youtube.com/watch?v=x_7YlGv9u1g', 'movie7.jpg', ''),
-(8, 'Ek Tha Tiger', 'Ek Tha Tiger (transl. Once There was a Tiger) is a 2012 Indian Hindi-language action thriller spy film directed and co-written by Kabir Khan from a story by and produced by Aditya Chopra. It is the first installment in the Tiger franchise and the first film in the YRF Spy Universe, and stars Salman Khan, Katrina Kaif, Ranvir Shorey, Girish Karnad, Roshan Seth, and Gavie Chahal.[4] In Ek Tha Tiger, Tiger (Khan), an Indian spy, is tasked with recovering information before it is gained by Pakistan, but is sidetracked after falling in love with Zoya (Kaif), a girl with a dark secret.\r\n\r\nEk Tha Tiger is the third collaboration between Kabir Khan and Yash Raj Films, after he directed Kabul Express (2006) and New York (2009). The film\'s production was extensive; the script underwent several re-writes, and production was delayed for a number of years prior to commencing in 2011.[5] Ek Tha Tiger was filmed mostly in India, Cuba, Turkey, Ireland and Thailand,', 'https://www.youtube.com/watch?v=SmUl0l8qBXw', 'movie8.jpg', '');
+(1, 'Black Adam', 'In ancient Kahndaq, Teth Adam was bestowed the almighty powers of the gods. After using these powers for vengeance, he was imprisoned, becoming Black Adam. Nearly 5,000 years have passed, and Black Adam has gone from man to myth to legend. Now free, his unique form of justice, born out of rage, is challenged by modern-day heroes who form the Justice Society: Hawkman, Dr. Fate, Atom Smasher and Cyclone.', 'https://www.youtube.com/watch?v=X0tOpBuYasI', 'Movie1.jpg', '7:30 PM'),
+(2, 'Cruella', 'Estella is a young and clever grifter who\'s determined to make a name for herself in the fashion world. She soon meets a pair of thieves who appreciate her appetite for mischief, and together they build a life for themselves on the streets of London. However, when Estella befriends fashion legend Baroness von Hellman, she embraces her wicked side to become the raucous and revenge-bent Cruella.', 'https://www.youtube.com/watch?v=gmRKv7n2If8', 'Movie2.jpg', '10:30 AM'),
+(3, 'Projek: High Council', 'This drama tells the story of gangsterism and bullying in a boarding school', 'https://www.youtube.com/watch?v=facWOae0GGg', 'Movie3.jpg', '10:00 PM'),
+(4, 'Wednesday', 'A sleuthing, supernaturally infused mystery charting Wednesday Addams\' years as a student at Nevermore Academy. Wednesday\'s attempts to master her emerging psychic ability, thwart a monstrous killing spree that has terrorized the local town, and solve the supernatural mystery that embroiled her parents 25 years ago - all while navigating her new and very tangled relationships at Nevermore', 'https://www.youtube.com/watch?v=Di310WS8zLk', 'Movie4.jpg', '12:30 PM'),
+(5, 'If I Stay', 'Mia Hall (Chloë Grace Moretz), a talented young cellist, thought the most difficult decision she would ever have to make would be whether to pursue her musical dreams at prestigious Juilliard or follow her heart to be with the love of her life, Adam (Jamie Blackley), a rock singer/guitarist. However, a car wreck changes everything in an instant, and now Mia\'s life hangs in the balance. Suspended between life and death, Mia faces a choice that will decide her future.', 'https://www.youtube.com/watch?v=rMp896hfp74', 'Movie5.jpg', '12:30 AM'),
+(6, 'Puss In Boots: The Last Wish', 'Puss in Boots discovers that his passion for adventure has taken its toll: he has burnt through eight of his nine lives. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives.', 'https://www.youtube.com/watch?v=RqrXhwS33yc', 'Movie6.jpg', '3:30 PM'),
+(7, 'Avatar: The Way of Water', 'Jake Sully and Ney\'tiri have formed a family and are doing everything to stay together. However, they must leave their home and explore the regions of Pandora. When an ancient threat resurfaces, Jake must fight a difficult war against the humans.', 'https://www.youtube.com/watch?v=o5F8MOz_IDw', 'Movie7.jpg', '9:30 PM'),
+(8, 'Thor: Love and Thunder', 'Thor embarks on a journey unlike anything he\'s ever faced -- a quest for inner peace. However, his retirement gets interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods. To combat the threat, Thor enlists the help of King Valkyrie, Korg and ex-girlfriend Jane Foster, who -- to his surprise -- inexplicably wields his magical hammer. Together, they set out on a harrowing cosmic adventure to uncover the mystery of the God Butcher\'s vengeance.', 'https://www.youtube.com/watch?v=Go8nTmfrQd8', 'Movie8.jpg', '9:45 PM'),
+(12, 'Mechamato', 'Bapak Boboiboy', 'https://www.youtube.com/watch?v=kjfNI9XOo5Y&t=51s', 'Movie9.jpg', '1.30 PM');
 
 -- --------------------------------------------------------
 
@@ -136,8 +137,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `mobile`) VALUES
-(2, 'test user', 'test@gmail.com', 'test@123', 8888888889),
-(4, 'Ajay', 'ajay@gmail.com', 'ajay@123', 48787878787);
+(2, 'Hilmieee', 'hilmie@gmail.com', 'mieee123', 8888888889),
+(4, 'Sara', 'sara@gmail.com', 'sara123', 48787878787),
+(6, 'Danny', 'danny@gmail.com', 'danny123', 112233),
+(7, 'Rep', 'Rep@gmail.com', 'rep123', 12112233);
 
 --
 -- Indexes for dumped tables
@@ -187,13 +190,13 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `shows`
@@ -205,9 +208,5 @@ ALTER TABLE `shows`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
